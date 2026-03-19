d3e9be41b51de73763a23cc9e5ef6d2c5e84fe39
gizul25 <gizul25@student.sdu.dk>
2026-03-19 10:01:41 +0000

    calculator

--- Application/ViewModels/MainWindowViewModel.cs
+++ Application/ViewModels/MainWindowViewModel.cs
@@ -1,6 +1,51 @@
-﻿namespace MyAvaloniaApp.ViewModels;
+﻿using System;
+using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using Avalonia.Controls;
+using Avalonia.Interactivity;
+using Avalonia.Media;
+
+namespace MyAvaloniaApp.ViewModels;
 
 public partial class MainWindowViewModel : ViewModelBase
 {
-    public string Greeting { get; } = "Welcome to Avalonia!";
+    [ObservableProperty]
+    private int _result = 0;
+
+    [ObservableProperty]
+    private int _firstOperand = 0;
+
+    [NotifyCanExecuteChangedFor(nameof(DivideCommand))]
+    [ObservableProperty]
+    private int _secondOperand = 0;
+
+
+    [RelayCommand]
+    private void Add()
+    {
+        Result = FirstOperand + SecondOperand;
+    }
+
+    [RelayCommand]
+    private void Substract()
+    {
+        Result = FirstOperand - SecondOperand;
+    }
+
+    [RelayCommand]
+    private void Multiply()
+    {
+        Result = FirstOperand * SecondOperand;
+    }
+
+    [RelayCommand(CanExecute = nameof(CanDivide))]
+    private void Divide()
+    {
+        Result = FirstOperand / SecondOperand;
+    }
+
+    private bool CanDivide()
+    {
+        return SecondOperand != 0;
+    }
 }
