82e5d40400d55b70f48b548ab1fb1798018cbefc
gizul25 <gizul25@student.sdu.dk>
2026-03-19 08:18:11 +0000

    init commit

---
+++ ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,6 @@
+﻿namespace AvaloniaAppTemplate.ViewModels;
+
+public partial class MainWindowViewModel : ViewModelBase
+{
+    public string Greeting { get; } = "Welcome to Avalonia!";
+}
---
+++ ViewModels/ViewModelBase.cs
@@ -0,0 +1,7 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace AvaloniaAppTemplate.ViewModels;
+
+public abstract class ViewModelBase : ObservableObject
+{
+}
